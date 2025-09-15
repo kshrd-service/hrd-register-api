cf4f417ec76e71d225204702cd2359df931062da
Sovannakk <sovannak.khengg@gmail.com>
2025-09-15 21:52:13 +0700

    finished

--- src/main/java/kh/com/kshrd/hrdregisterapi/HrdRegisterApiApplication.java
+++ src/main/java/kh/com/kshrd/hrdregisterapi/HrdRegisterApiApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,7 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(info = @Info(title = "HRD Register Api",
         version = "v1",
-        description = "This is description"))
+        description = "This is description"), servers = @Server(url = "/"))
 @SecurityScheme(
         name = "hrd",
         type = SecuritySchemeType.HTTP,
